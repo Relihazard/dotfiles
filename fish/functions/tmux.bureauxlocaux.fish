b0ffa98d082ac10d1782a00e1b79e2ca7a74b748
Axel Grossmann <relihazard@gmail.com>
2021-12-21 21:18:45 +0100

    placeholder

--- fish/functions/tmux.bureauxlocaux.fish
+++ fish/functions/tmux.bureauxlocaux.fish
@@ -1,17 +1,17 @@
-#!/bin/zsh
+function tmux.bureauxlocaux
 tmux new-session -d -s "bureauxlocaux" -n "docker" -c "$HOME/Projects/bureauxlocaux"
 tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "npm"
 tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "shell"
 tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "git"
-tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "python-shell"
 tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "tests"
 tmux new-window -c "$HOME/Projects/bureauxlocaux" -n "ssh"
 tmux send-keys -t "bureauxlocaux:docker" "docker-compose up -d" Enter
 tmux send-keys -t "bureauxlocaux:npm" ". venv/bin/activate.fish" Enter "source .env" Enter "clear" Enter "npm run watch-dev" Enter
-tmux send-keys -t "bureauxlocaux:shell" ". venv/bin/activate" Enter "source .env" Enter "clear" Enter
-tmux send-keys -t "bureauxlocaux:git" ". venv/bin/activate" Enter "source .env" Enter "clear" Enter
-tmux send-keys -t "bureauxlocaux:tests" ". venv/bin/activate" Enter "source .env" Enter "clear" Enter
+tmux send-keys -t "bureauxlocaux:shell" ". venv/bin/activate.fish" Enter "source .env" Enter "code ." Enter "clear" Enter
+tmux send-keys -t "bureauxlocaux:git" ". venv/bin/activate.fish" Enter "source .env" Enter "clear" Enter
+tmux send-keys -t "bureauxlocaux:tests" ". venv/bin/activate.fish" Enter "source .env" Enter "clear" Enter
 tmux send-keys -t "bureauxlocaux:ssh" "ssh bltest" Enter "clear" Enter
 tmux select-window -t "bureauxlocaux:shell"
 tmux select-pane -t 1
 tmux attach-session -t "bureauxlocaux"
+end
